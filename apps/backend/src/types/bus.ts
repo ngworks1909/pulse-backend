8a3dd7c915002c2c062604d9174ea8e5bae6182b
ngworks1909 <nithin.kanduru1908@gmail.com>
2025-11-22 00:10:30 +0530

    crons

---
+++ apps/backend/src/types/bus.ts
@@ -0,0 +1,16 @@
+interface Bus{
+    operatorId: number;
+    travelsName: string;
+    busType: string;
+    busTypeId: number;
+    totalRatings: number;
+    numberOfReviews: number;
+    departureTime: string;
+    arrivalTime: string;
+    journeyDurationMin: number;
+    standardBpName: string;
+    standardpName: string;
+    isAc: boolean;
+    isSleeper: boolean;
+    fareList: number[];
+}
